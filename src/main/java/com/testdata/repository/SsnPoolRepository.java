b894c8db74f61eff9e2376e04956a2bb55c92e00
GyanendraB <48312955+GyanendraB@users.noreply.github.com>
2026-03-11 00:50:31 +0530

    Auto-generate SSNs when pool is exhausted

--- src/main/java/com/testdata/repository/SsnPoolRepository.java
+++ src/main/java/com/testdata/repository/SsnPoolRepository.java
@@ -7,4 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface SsnPoolRepository extends JpaRepository<SsnPool, String> {
 
     Optional<SsnPool> findFirstByUsedFalse();
+
+    Optional<SsnPool> findTopByOrderBySsnDesc();
 }
