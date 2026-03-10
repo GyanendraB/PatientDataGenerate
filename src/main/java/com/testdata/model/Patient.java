fba5b1b4f8c34c774878844b1cec37f02a616126
GyanendraB <48312955+GyanendraB@users.noreply.github.com>
2026-03-10 23:25:07 +0530

    Remove Lombok dependency by implementing Patient model manually

--- src/main/java/com/testdata/model/Patient.java
+++ src/main/java/com/testdata/model/Patient.java
@@ -1,12 +1,5 @@
 package com.testdata.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class Patient {
 
     private String patientId;
@@ -18,4 +11,97 @@ public class Patient {
     private String status;
     private String department;
     private String bedNumber;
+
+    public Patient() {
+    }
+
+    public Patient(String patientId, String name, String dob, String phone, String ssn, String status,
+                   String department, String bedNumber) {
+        this.patientId = patientId;
+        this.name = name;
+        this.dob = dob;
+        this.phone = phone;
+        this.ssn = ssn;
+        this.status = status;
+        this.department = department;
+        this.bedNumber = bedNumber;
+    }
+
+    public String getPatientId() {
+        return patientId;
+    }
+
+    public void setPatientId(String patientId) {
+        this.patientId = patientId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getBedNumber() {
+        return bedNumber;
+    }
+
+    public void setBedNumber(String bedNumber) {
+        this.bedNumber = bedNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Patient{" +
+                "patientId='" + patientId + '\'' +
+                ", name='" + name + '\'' +
+                ", dob='" + dob + '\'' +
+                ", phone='" + phone + '\'' +
+                ", ssn='" + ssn + '\'' +
+                ", status='" + status + '\'' +
+                ", department='" + department + '\'' +
+                ", bedNumber='" + bedNumber + '\'' +
+                '}';
+    }
 }
